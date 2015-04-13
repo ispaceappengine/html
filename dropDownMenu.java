93cb09ff08967433ef34131181219be7f0511f22
geronimoooooooo <geronimoooooooo@gmail.com>
2015-04-13 17:55:49 +0200

    Create dropDownMenu.java

---
+++ dropDownMenu.java
@@ -0,0 +1,3 @@
+how to display selected options (drop down menu) in jsp?
++ http://stackoverflow.com/questions/2407945/how-to-get-multiple-selected-values-from-select-box-in-jsp
++ http://stackoverflow.com/questions/13064141/how-to-display-selected-options-drop-down-menu-in-jsp
