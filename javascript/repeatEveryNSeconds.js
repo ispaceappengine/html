4c423d833b841291e19528300ed39e65c79cbb08
geronimoooooooo <geronimoooooooo@gmail.com>
2016-04-11 04:09:06 +0200

    Update repeatEveryNSeconds.js

--- javascript/repeatEveryNSeconds.js
+++ javascript/repeatEveryNSeconds.js
@@ -1,4 +1,13 @@
-script type="text/javascript">
+<script type="text/javascript">
+    var intervalId = 0;
+    intervalId = setInterval(myFunc, 3000);
+
+function myFunc(){
+ $("#pid1").text("bla")
+}
+</script>
+------------------------------------------------------------
+<script type="text/javascript">
     function crunchifyAjax() {
         $.ajax({
             url : 'ajaxtest.html',
