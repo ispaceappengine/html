8d3340164884140515459bdd717a2c0ed9c36fd4
geronimoooooooo <geronimoooooooo@gmail.com>
2016-04-18 00:50:31 +0200

    Update json_parser.js

--- json_parser.js
+++ json_parser.js
@@ -37,3 +37,23 @@ returnedData.type => FeatureCollection
 returnedData.features[0].geometry.coordinates => 13.039265,47.82089
 returnedData.features[1].geometry.coordinates => 102,0,103,1,104,0,105,1
 returnedData.features[1].geometry.coordinates[0] => 102,0
+
+
+$.each(returnedData.features, function(index, element) {
+        $('body').append($('<div>', {
+            text: element.type
+        }));
+    });
+=>Feature
+Feature
+Feature
+
+
+$.each(returnedData.features, function(index, element) {
+        $('body').append($('<div>', {
+            text: element.geometry.type
+        }));
+    });
+=> Point
+LineString
+Polygon
