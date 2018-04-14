33e561f7089fc9ff8a7c4a8d56d3ff1d70dcc774
geronimoooooooo <geronimoooooooo@gmail.com>
2018-04-14 14:04:30 +0200

    Update Events.js

--- ng/Events.js
+++ ng/Events.js
@@ -1,6 +1,13 @@
-<joke-form 
-  (jokeCreated)="addJoke($event)"> ②
-</joke-form>
+<joke-form   (jokeCreated)="addJoke($event)"> </joke-form>
 
 The component is going to emit an event called jokeCreated whenever a user clicks the Create
 button. When the jokeCreated event is emitted I want to call the addJoke function.
+
+//output an event in function createJoke()
+class JokeFormComponent {
+  @Output() jokeCreated = new EventEmitter<Joke>();
+
+  createJoke() {
+  this.jokeCreated.emit(new Joke("A setup", "A punchline"));
+  }
+}
