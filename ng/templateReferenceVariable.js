8abe496587d0dee8a4d83a97eade2959fa7d0090
geronimoooooooo <geronimoooooooo@gmail.com>
2018-04-14 14:24:58 +0200

    Create templateReferenceVariable.js

---
+++ ng/templateReferenceVariable.js
@@ -0,0 +1,13 @@
+<input type="text"
+  class="form-control"
+  placeholder="Enter the setup"
+  #setup>
+  
+<button type="button"
+  class="btn btn-primary"
+  (click)="createJoke(setup.value, punchline.value)">Create
+</button>
+
+createJoke(setup: string, punchline: string) {
+  this.jokeCreated.emit(new Joke(setup, punchline));
+}
