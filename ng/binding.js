d8abf5fe594039086fd1eb334aa007bc784861ce
geronimoooooooo <geronimoooooooo@gmail.com>
2018-04-22 19:02:32 +0200

    Update binding.js

--- ng/binding.js
+++ ng/binding.js
@@ -3,3 +3,12 @@ class JokeComponent {
 }
 
 <joke *ngFor="let j of jokes" [joke]="j"></joke>
+-----------------------------
+<header (loginChanged)="loggedIn = $event"></header>
+<sidebar (searchTermChanged)="searchTerm = $event"></sidebar>
+<content [searchTerm]="searchTerm"></content>
+------------------------------
+------------------------------
+------------------------------
+-------------------------------
+------------------------------
