f3148386a11fd543bc8887c8195917b26395e90e
geronimoooooooo <geronimoooooooo@gmail.com>
2018-04-22 20:19:07 +0200

    Update Decorators.ts

--- ts/Decorators.ts
+++ ts/Decorators.ts
@@ -1,4 +1,7 @@
+<h4 #header>View Jokes</h4>
+
 class JokeListComponent {
   //instance of the child components that exists inside this components view
   @ViewChild(JokeComponent) jokeViewChild: JokeComponent;
   @ViewChildren(JokeComponent) jokeViewChildren: QueryList<JokeComponent>; //toArray possible
+  @ViewChild("header") headerEl: ElementRef; //reference to template variable
