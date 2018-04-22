7108fa21da584672589f0092bdd37d1fd87dbd94
geronimoooooooo <geronimoooooooo@gmail.com>
2018-04-22 20:12:37 +0200

    Update Decorators.ts

--- ts/Decorators.ts
+++ ts/Decorators.ts
@@ -1,3 +1,4 @@
 class JokeListComponent {
   //instance of the child components that exists inside this components view
   @ViewChild(JokeComponent) jokeViewChild: JokeComponent;
+  @ViewChildren(JokeComponent) jokeViewChildren: QueryList<JokeComponent>; //toArray possible
