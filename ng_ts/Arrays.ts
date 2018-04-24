7c63494891abbfc478e0bc406f7b3619a79a26a7
geronimoooooooo <geronimoooooooo@gmail.com>
2018-04-24 13:47:19 +0200

    Update Arrays.ts

--- ng_ts/Arrays.ts
+++ ng_ts/Arrays.ts
@@ -3,3 +3,18 @@ skills: Array = new Array();
 skills: string[] = ['Java', 'Angular'];
 skills: Array = ['Java', 'Angular'];
 this.skills.push('.Net');
+-----------------------------------------
+  public static Products: Product[] = [
+{
+'id': 11,
+'title': 'OPPO F5 Youth (Gold, 32 GB)',
+'price': 16990
+},
+{
+'id': 12,
+'title': 'Dell Inspiron 7000',
+'price': 59990
+}
+];
+-----------------------------------------
+  
