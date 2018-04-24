4b9ac0c0b045c50d033cfe2db01577900e971fe0
geronimoooooooo <geronimoooooooo@gmail.com>
2018-04-24 14:45:46 +0200

    Update Decorators.ts

--- ng_ts/Decorators.ts
+++ ng_ts/Decorators.ts
@@ -5,3 +5,9 @@ class JokeListComponent {
   @ViewChild(JokeComponent) jokeViewChild: JokeComponent;
   @ViewChildren(JokeComponent) jokeViewChildren: QueryList<JokeComponent>; //toArray possible
   @ViewChild("header") headerEl: ElementRef; //reference to template variable
+-----------------------------------------
+class ProductComponent{
+  @Input() product: Product;
+...
+}
+-----------------------------------------
