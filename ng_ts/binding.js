14cd15565a06b1d724a2be98cadc2d06603b4b6c
geronimoooooooo <geronimoooooooo@gmail.com>
2018-04-24 14:43:23 +0200

    Update binding.js

--- ng_ts/binding.js
+++ ng_ts/binding.js
@@ -3,17 +3,18 @@ class JokeComponent {
 }
 
 <joke *ngFor="let j of jokes" [joke]="j"></joke>
------------------------------
+------------------------------------------------------------
 <header (loginChanged)="loggedIn = $event"></header>
 <sidebar (searchTermChanged)="searchTerm = $event"></sidebar>
 <content [searchTerm]="searchTerm"></content>
-------------------------------
+-------------------------------------------------------------
 <strong ngNonBindable> {{(5*5+1)/2}} </strong>
-------------------------------
+-------------------------------------------------------------
 products: Product[] = [];
 constructor() {
     this.products = MockData.Products;
 }
-------------------------------
--------------------------------
-------------------------------
+-------------------------------------------------------------
+    <app-product class="col-md-3" [product]="p" *ngFor="let p of products"></app-product>
+--------------------------------------------------------------
+-------------------------------------------------------------
