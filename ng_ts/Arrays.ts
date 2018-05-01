be34384831b497e4b307ff0e5678f2e4f11504c6
geronimoooooooo <geronimoooooooo@gmail.com>
2018-05-01 17:18:36 +0200

    Update Arrays.ts

--- ng_ts/Arrays.ts
+++ ng_ts/Arrays.ts
@@ -7,14 +7,47 @@ this.skills.push('.Net');
   public static Products: Product[] = [
 {
 'id': 11,
-'title': 'OPPO F5 Youth (Gold, 32 GB)',
-'price': 16990
+'title': 'OPPO F5 Youth (Gold, 32 GB)'
 },
 {
 'id': 12,
-'title': 'Dell Inspiron 7000',
-'price': 59990
+'title': 'Dell Inspiron 7000'
 }
 ];
 -----------------------------------------
-  
+peopleByCountry: any[] = [
+{
+  'country': 'UK',
+  'people': [
+    {
+    "name": "Douglas Pace"
+    },
+    {
+     "name": "Mcleod Mueller"
+    },
+  ]
+},
+{
+  'country': 'US',
+  'people': [
+  {
+  "name": "Day Meyers"
+  },
+  {
+  "name": "Aguirre Ellis"
+  },
+  {
+  "name": "Cook Tyson"
+  }
+  ]
+}
+];
+
+<ul *ngFor="let group of peopleByCountry"> ①
+<li>{{ group.country }}</li>
+<ul>
+<li *ngFor="let person of group.people"> ②
+{{ person.name }}
+</li>
+</ul>
+</ul>
